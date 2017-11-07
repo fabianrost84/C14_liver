d343f556b6ca62f26b8a30251fb08db92dd655e7
fabianrost <fabianrost@mailbox.org>
2017-11-07 14:11:06 +0100

    First fit of the liver data with Scenario A

--- atm_c14_data.py
+++ atm_c14_data.py
@@ -3,16 +3,16 @@ from scipy import interpolate
 import pandas as pd
 
 tlag = 1.0
-c14_data_file = './spalding_2013_data.pkl'
+_c14_data_file = 'data/14C_levin_data_until_2016_for_plotting.xlsx'
 
-c14_data = pd.read_excel('data/14C_levin_data_until_2016_for_plotting.xlsx', names=['year', 'd14C'])
+c14_data = pd.read_excel(_c14_data_file, names=['year', 'd14C'])
 
 last_c14 = c14_data['d14C'].loc[c14_data['year'].argmax()]
 extrapolate_value =[2020.0, last_c14]
 
 c14_data.loc['extrapolate_value'] = extrapolate_value
 
-c14_data = c14_data.sort('year')
+c14_data = c14_data.sort_values(by='year')
 c14_data['d14C'] /= 1000.0
 
 def interpolate(xp, fp):
@@ -20,8 +20,8 @@ def interpolate(xp, fp):
 
 
 K = interpolate(c14_data['year'], c14_data['d14C'])
-K.__doc__ ='Interpolation function for the atmospheric C14 data extracted from Spalding et al., 2013.' 
+K.__doc__ ='Interpolation function for the atmospheric C14 data I got from Paula' 
 
 
 Klag = interpolate(c14_data['year'] + tlag, c14_data['d14C'])
-Klag.__doc__ ='Interpolation function for the atmospheric C14 data extracted from Spalding et al., 2013., shifted by tlag' 
+Klag.__doc__ ='Interpolation function for the atmospheric C14 data I got from Paula' 
